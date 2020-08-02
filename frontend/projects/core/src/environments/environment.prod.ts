feb07b88fd69383f80b9569960b925dfb3fc802f
Dmitry Zharikov <zdimon77@gmail.com>
2020-08-02 15:10:41 +0300

    commit from zdimon77@gmail.com ----

--- frontend/projects/core/src/environments/environment.prod.ts
+++ frontend/projects/core/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   backendUrl: 'https://chat.webmonstr.com/',
   centUrl: 'wss://chat.webmonstr.com:9999/connection/websocket',
-  onlineUrl: 'ws://chat.webmonstr.com/online/',
+  onlineUrl: 'wss://chat.webmonstr.com/online/',
   centToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0MiJ9.H9s-s6jxXo4jK6zJLEnp73S9hss94TEoSPXPnoikeRs'
 };
