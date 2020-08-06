d0da362bcd2429e9738e8d0a66837db880b8ff0a
Your Name <you@example.com>
2020-08-06 16:46:27 +0300

    smile + sticker

--- backend/backend/cent_client.py
+++ backend/backend/cent_client.py
@@ -2,13 +2,13 @@ from cent import Client
 
 from backend.settings import CENT_URL, CENT_KEY
 
-
 class CentClient(object):
 
     def __init__(self):
         print('Init connection')
-        self.con = Client(CENT_URL, api_key=CENT_KEY, timeout=1)
+        self.con = Client(CENT_URL,CENT_KEY, timeout=1)
 
     def send(self,token,message):
         self.con.publish(token, message)
+        #print(" HISTORY OF MESSAGES : ", self.con.history(token))
 
