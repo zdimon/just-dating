168ee1f71446857eb7a43ea83c7a585921d2a11a
Your Name <you@example.com>
2021-04-12 23:05:02 +0300

    commit from zdimon77@gmail.com ----

--- tmp/quiz/management/commands/create_quize.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.core.management.base import BaseCommand, CommandError
-import os
-import json
-from quiz.utils import create_test_quize
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        create_test_quize()
