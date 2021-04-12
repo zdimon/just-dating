9d6f2043eec3e6f68e3adc45341219b830a3f987
Your Name <you@example.com>
2021-04-12 22:19:21 +0300

    commit from zdimon77@gmail.com ----

--- frontend/projects/core/src/environments/environment.prod.ts
+++ frontend/projects/core/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://chat.webmonstr.com/',
+  backendUrl: '/',
   centUrl: 'wss://chat.webmonstr.com/connection/websocket',
   onlineUrl: 'wss://chat.webmonstr.com/online/',
   centToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0MiJ9.H9s-s6jxXo4jK6zJLEnp73S9hss94TEoSPXPnoikeRs'
